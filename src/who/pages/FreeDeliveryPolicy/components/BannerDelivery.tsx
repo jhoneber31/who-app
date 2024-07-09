8023cab7dc55d4f5e57e6b9139d3102893e675ef
jhoneber31 <jhoneber31@gmail.com>
2024-07-09 02:30:24 -0500

    free delivery page

--- src/who/pages/FreeDeliveryPolicy/components/BannerDelivery.tsx
+++ src/who/pages/FreeDeliveryPolicy/components/BannerDelivery.tsx
@@ -2,7 +2,9 @@
 export const BannerDelivery = () => {
   return (
     <section>
-
+      <div>
+        <img src="https://cdn.shopify.com/s/files/1/3009/5686/files/Free_Delivery_Support_2048x2048.jpg?v=1526118558" alt="banner-delivery" className="object-fill object-center w-full" />
+      </div>
     </section>
   )
 }
